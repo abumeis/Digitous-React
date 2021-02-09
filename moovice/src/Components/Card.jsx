abdd50edcaa0e6391bda1e3b9498f1e68fadfe9e
abumeis <37344104+abumeis@users.noreply.github.com>
2021-02-09 21:30:01 +0100

    4em étape finish

--- moovice/src/Components/Card.jsx
+++ moovice/src/Components/Card.jsx
@@ -2,22 +2,12 @@ import React from 'react';
 
 class Card extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          image: "/img/filmBg.png" ,
-          titre: '',
-          annéeDeSortie: "",
-          discription: '',
-        };
-      }
 
 
  
     
   render() {
-    //const picture = `https://image.tmdb.org/t/p/w300${this.props.poster_path}`;
-    //console.log('this.props', this.props);
+ 
     return (
       <div>
          
